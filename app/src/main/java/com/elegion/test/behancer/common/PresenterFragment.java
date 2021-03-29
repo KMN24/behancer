53a68cecd9960fd3fe6d5b14746c75562149dde7
Mederbek <kanatbekov.meder.00@gmail.com>
2021-03-29 18:14:34 +0600

    created base classes of MVP

---
+++ app/src/main/java/com/elegion/test/behancer/common/PresenterFragment.java
@@ -0,0 +1,19 @@
+package com.elegion.test.behancer.common;
+
+import androidx.fragment.app.Fragment;
+
+public abstract class PresenterFragment<P extends BasePresenter> extends Fragment {
+
+    protected abstract P getPresenter();
+
+    @Override
+    public void onDetach() {
+
+        if (getPresenter() != null){
+            getPresenter().disposeAll();
+        }
+
+        super.onDetach();
+
+    }
+}
